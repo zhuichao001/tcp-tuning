31e306d6cb69107ef8c446fadd3633a92285d9fe
qiuzerzhang <qiuzerzhang@tencent.com>
2020-08-02 07:24:26 +0800

    TCP快速打开

--- tcp_fastopen/tfo_client.c
+++ tcp_fastopen/tfo_client.c
@@ -9,22 +9,31 @@
 #include<arpa/inet.h>
 
 
+struct sockaddr_in * gen_svraddr(const char *ip, const int port){
+    struct sockaddr_in *svraddr = (struct sockaddr_in *)malloc(sizeof(struct sockaddr_in));
+    memset(svraddr, 0, sizeof(struct sockaddr_in));
+    svraddr->sin_family = AF_INET;
+    svraddr->sin_addr.s_addr = inet_addr(ip);
+    svraddr->sin_port = htons(port);
+    return svraddr;
+}
+
+
+void print_errno(const char * prefix){
+     printf("%s socket error: %s(errno: %d)\n", prefix, strerror(errno), errno);
+     exit (0);
+}
+
+
 void tfo_client(){
     int sockfd;
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf ("create socket error: %s(errno: %d)\n", strerror (errno), errno);
-        exit (0);
+        print_errno("create");
     }
  
-    struct sockaddr_in servaddr;
-    memset (&servaddr, 0, sizeof (servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    servaddr.sin_port = htons(8764);
- 
-    char buf[] = "hello, this is tcp fast open test data...";
-    int ret = sendto(sockfd, buf, strlen(buf), MSG_FASTOPEN, (struct sockaddr *)&servaddr, sizeof(servaddr));
-    if (ret < 0) {
+    struct sockaddr_in * svraddr = gen_svraddr("127.0.0.1", 8766); 
+    char buf[] = "hello, coming TFO data.";
+    if (sendto(sockfd, buf, strlen(buf), MSG_FASTOPEN, (struct sockaddr *)svraddr, sizeof(*svraddr)) < 0) {
         printf ("send msg error: %s(errno: %d)\n", strerror (errno), errno);
     }
     close(sockfd);
--- tcp_fastopen/tfo_server.c
+++ tcp_fastopen/tfo_server.c
@@ -10,38 +10,46 @@
 #include <arpa/inet.h>
 
 
+struct sockaddr_in * gen_svraddr(const char *ip, const int port){
+    struct sockaddr_in *svraddr = (struct sockaddr_in *)malloc(sizeof(struct sockaddr_in));
+    memset (svraddr, 0, sizeof(struct sockaddr_in));
+    svraddr->sin_family = AF_INET;
+    svraddr->sin_addr.s_addr = inet_addr(ip);
+    svraddr->sin_port = htons(port);
+    return svraddr;
+}
+
+
+void print_errno(const char * prefix){
+     printf("%s socket error: %s(errno: %d)\n", prefix, strerror(errno), errno);
+     exit (0);
+}
+
+
 void tfo_server(){
     int listenfd;
     if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        printf("create socket error: %s(errno: %d)\n", strerror(errno), errno);
-        exit (0);
+        print_errno("create");
     }
  
-    struct sockaddr_in servaddr;
-    memset (&servaddr, 0, sizeof (servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    servaddr.sin_port = htons (8764);
- 
-    if (bind(listenfd, (struct sockaddr *) &servaddr, sizeof (servaddr)) == -1) {
-        printf("bind socket error: %s(errno: %d)\n", strerror(errno), errno);
-        exit(0);
+    struct sockaddr_in * svraddr = gen_svraddr("127.0.0.1", 8766); 
+    if (bind(listenfd, (struct sockaddr *)svraddr, sizeof (struct sockaddr_in)) == -1) {
+        print_errno("bind");
     }
  
     int backlog = 10;
     if (listen(listenfd, backlog) == -1) {
-        printf("listen socket error: %s(errno: %d)\n", strerror(errno), errno);
-        exit(0);
+        print_errno("listen");
     }
  
     int qlen = 5;
     if (setsockopt(listenfd, IPPROTO_TCP, TCP_FASTOPEN, &qlen, sizeof(qlen)) < 0) {
-        printf("setsockopt error: %s(errno: %d)\n", strerror(errno), errno);
+        print_errno("setsockopt");
     }
 
+    int fd;
     while (1) {
-        printf("Before accpet!\n");
-        int fd;
+        printf("Before accpet.\n");
         if ((fd = accept (listenfd, (struct sockaddr *) NULL, NULL)) == -1) {
             printf("accept error: %s(errno: %d)\n", strerror(errno), errno);
         }
@@ -51,7 +59,6 @@ void tfo_server(){
         printf("=====> recv: %s\n", recv_buf);
         close(fd);
     }
-
 }
 
 
