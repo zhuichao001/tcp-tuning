dbd0203a6b135f4416f7e11c4f86b8ec579342ac
qiuzerzhang <qiuzerzhang@tencent.com>
2020-08-02 19:17:07 +0800

    modify:全连接队列 server监听逻辑

--- backlog/server.py
+++ backlog/server.py
@@ -2,19 +2,27 @@ import socket
 import threading
 import time
 
-def handle_client(client_socket):
+def handle_conn(client_socket):
     request = client_socket.recv(1024)
     print("[*] Received: %s" % request)
     client_socket.send("hi~".encode())
     client_socket.close()
 
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind(("0.0.0.0", 8723))
-print "[*] Listening on :8723"
-server.listen(1024) #backlog=8
-time.sleep(120)
-while True:
-    client, addr = server.accept()
-    print "[*] Acception connection from %s:%d" % (addr[0], addr[1]) 
-    th = threading.Thread(target=handle_client, args=(client,))
-    th.start()
+
+def run():
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server.bind(("0.0.0.0", 8723))
+    print "[*] Listening on :8723"
+
+    server.listen(1024) #backlog=1024
+    time.sleep(120)
+
+    while True:
+        client, addr = server.accept()
+        print "[*] Acception connection from %s:%d" % (addr[0], addr[1]) 
+        th = threading.Thread(target=handle_conn, args=(client,))
+        th.start()
+
+
+if __name__ == '__main__':
+    run()
