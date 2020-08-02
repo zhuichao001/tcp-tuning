a412e75929e30fee5e026ddfb0551721f54c4fa8
qiuzerzhang <qiuzerzhang@tencent.com>
2020-08-02 18:49:15 +0800

    optimize：keepalive自动保活机制

--- keepalive/client.c
+++ keepalive/client.c
@@ -11,7 +11,7 @@
 #include <arpa/inet.h>
 
 
-void set_sock_keepalive(int sockfd, int idle, int interval, int count){
+void set_sock_keepalive(int sockfd, int idle, int interval, int count) {
     int open = 1;
     setsockopt(sockfd, SOL_SOCKET, SO_KEEPALIVE, &open, sizeof(open));
     setsockopt(sockfd, SOL_TCP, TCP_KEEPIDLE, &idle, sizeof(idle));
@@ -20,7 +20,7 @@ void set_sock_keepalive(int sockfd, int idle, int interval, int count){
 }
 
 
-struct sockaddr_in * gen_svraddr(const char *ip, const int port){
+struct sockaddr_in * gen_svraddr(const char *ip, const int port) {
     struct sockaddr_in *svraddr = (struct sockaddr_in *)malloc(sizeof(struct sockaddr_in));
     memset (svraddr, 0, sizeof(struct sockaddr_in));
     svraddr->sin_family = AF_INET;
@@ -30,15 +30,13 @@ struct sockaddr_in * gen_svraddr(const char *ip, const int port){
 }
 
 
-void print_errno(const char * prefix){
+void print_errno(const char * prefix) {
      printf("%s socket error: %s(errno: %d)\n", prefix, strerror(errno), errno);
      exit (0);
 }
 
 
 int main(int argc, char *argv[]) {
-    struct hostent *he;
-
     int sockfd;  
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         print_errno("socket");
@@ -53,8 +51,8 @@ int main(int argc, char *argv[]) {
     }
 
     while (1) {
-        char line[] = "Hello, world!\n";
-        if (send(sockfd, line, sizeof(line), 0) == -1){
+        char line[] = "tcp tuning KEEP_ALIVE.";
+        if (send(sockfd, line, sizeof(line), 0) == -1) {
             print_errno("send");
         }
         printf("After sending:%s \n", line);
@@ -66,7 +64,8 @@ int main(int argc, char *argv[]) {
         }
 
         printf("[pid=%d], Receive: %s\n", getpid(), buf);
-        sleep(120);
+        const IDLE_SECONDS = 120;
+        sleep(IDLE_SECONDS);
     }
 
     close(sockfd);
