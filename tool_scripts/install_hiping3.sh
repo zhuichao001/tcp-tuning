c979e9d3e7e0d3478c2889a02d45dfd46604fe0b
qiuzerzhang <qiuzerzhang@tencent.com>
2020-08-02 09:34:20 +0800

    modify:安装hping3

--- tool_scripts/install_hiping3.sh
+++ tool_scripts/install_hiping3.sh
@@ -1,3 +1,6 @@
+
+sudo su
+
 yum install libpcap
 yum install libpcap-devel
 ln -sf /usr/include/pcap-bpf.h /usr/include/net/bpf.h
@@ -6,5 +9,4 @@ yum -y install tcl-devel
 git clone https://github.com/antirez/hping
 ./configure
 make
-sudo su
 make install
