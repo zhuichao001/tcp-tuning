a726f3194552981afbc13288a5e621fa17b4f3a6
zhuichao001 <zhuichao001@163.com>
2022-06-04 08:14:26 +0800

    update localip

--- localip/localip.cpp
+++ localip/localip.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <stdio.h>
 #include <net/if.h>
 #include <string.h>
@@ -6,14 +7,10 @@
 #include <sys/ioctl.h>
 #include <netinet/in.h>
 
-int get_localip(const char * eth_name, char *local_ip_addr) {
-    register int fd, intrface;
+int get_localip(const std::string eth_name, std::string &local_ip_addr) {
+    int fd, intrface;
     struct ifreq ifr[32];
     struct ifconf ifc;
-
-    if (local_ip_addr == NULL || eth_name == NULL) {
-        return -1;
-    }
 
     if ((fd=socket(AF_INET, SOCK_DGRAM, 0)) > 0) {
         ifc.ifc_len = sizeof ifr;
@@ -24,8 +21,8 @@ int get_localip(const char * eth_name, char *local_ip_addr) {
             while (intrface-- > 0) {
                 //Get IP Address
                 if (!(ioctl(fd, SIOCGIFADDR, (char*)&ifr[intrface]))) {
-                    if(strcmp(eth_name, ifr[intrface].ifr_name) == 0) {
-                        sprintf(local_ip_addr, "%s", inet_ntoa(((struct sockaddr_in*)(&ifr[intrface].ifr_addr))->sin_addr));
+                    if(strcmp(eth_name.c_str(), ifr[intrface].ifr_name) == 0) {
+                        local_ip_addr = inet_ntoa(((struct sockaddr_in*)(&ifr[intrface].ifr_addr))->sin_addr);
                         break;
                     }
                 }
@@ -40,11 +37,11 @@ int get_localip(const char * eth_name, char *local_ip_addr) {
 }
 
 int main(int argc, const char **argv) {
-    char local_ip[20] = {0};
+    std::string local_ip;
 
     int ret = get_localip("eth0", local_ip);
     if (ret == 0) {
-        printf("local ip:%s\n", local_ip);
+        printf("local ip:%s\n", local_ip.c_str());
     } else {
         printf("get local ip failure\n");
     }
